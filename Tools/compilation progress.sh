99af25398c2aae58024b7449451c20040f26792a
Sergio Melas <sergiomelas@gmail.com>
2026-03-28 00:11:20 +0100

    Delete Tools directory

--- Tools/compilation progress.sh
+++
@@ -1,6 +0,0 @@
-watch -n 5 '
-  total=$(find . -name "*.c" | wc -l);
-  current=$(find . -name "*.o" | wc -l);
-  percent=$((current * 100 / total));
-  echo "Kernel Compilation Progress: $percent% ($current of $total files)";
-'
